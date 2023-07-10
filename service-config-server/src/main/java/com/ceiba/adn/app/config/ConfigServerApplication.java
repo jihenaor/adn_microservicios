a7f4ccaebad616bcf04f0e5a555e45c8a2cce35e
jorghere <jihenaor@gmail.com>
2023-07-10 15:42:05 -0500

    Se ajusta configuracion para obtener datos de config server

--- service-config-server/src/main/java/com/ceiba/adn/app/config/ConfigServerApplication.java
+++ service-config-server/src/main/java/com/ceiba/adn/app/config/ConfigServerApplication.java
@@ -6,12 +6,12 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
-@EnableEurekaClient
+// @EnableEurekaClient
 @SpringBootApplication
-public class ServiceConfigServerApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceConfigServerApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
