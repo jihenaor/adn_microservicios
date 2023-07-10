523b243e8736519f12a2f3443ca3b6d45b48b19c
jorghere <jihenaor@gmail.com>
2023-07-10 17:17:48 -0500

    no message

--- service-config-server/src/main/java/com/ceiba/adn/app/config/ConfigServerApplication.java
+++ service-config-server/src/main/java/com/ceiba/adn/app/config/ConfigServerApplication.java
@@ -3,7 +3,7 @@ package com.ceiba.adn.app.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+// import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
 // @EnableEurekaClient
